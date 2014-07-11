3ba985dd6327de0d67be47f366d508722525b374
Christian Capil <christian.capil@outlook.com>
2014-07-11 12:14:30 -0700

    Initial Commit

---
+++ DisplayAlertError/App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+
+namespace DisplayAlertError
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			var button = new Button {
+				Text = "Show Alert",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			var content = new ContentPage {			
+				Content =	new StackLayout { 
+					Padding = new Thickness(10, 20),
+					Children = {
+						button,
+						new Label {
+							Text = "Forms Page!",
+							VerticalOptions = LayoutOptions.CenterAndExpand,
+							HorizontalOptions = LayoutOptions.CenterAndExpand,
+						}
+					}
+				}
+			};
+
+			button.Clicked += (sender, e) => content.DisplayAlert ("Test alert", "Test alert message", "Ok", "Cancel");;
+
+			return content;
+		}
+	}
+}
+
---
+++ iOS/TestDialogController.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+
+namespace DisplayAlertError.iOS
+{
+	public partial class TestDialogController : UIViewController
+	{
+		public TestDialogController () : base ("TestDialogController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			var button = new UIButton(new RectangleF (0, 120, 320, 44));
+
+			button.SetTitle ("Push forms", UIControlState.Normal);
+			button.BackgroundColor = UIColor.Blue;
+			var label = new UILabel (new RectangleF (0, 90, 320, 44));
+
+			label.Text = "Test Label";
+
+			button.TouchUpInside += (sender, e) => {
+				NavigationController.PushViewController(DisplayAlertError.App.GetMainPage().CreateViewController(), true);
+			};
+			View.Add (label);
+
+			View.Add (button);
+
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
